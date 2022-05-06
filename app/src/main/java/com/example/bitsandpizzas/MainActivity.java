638784fb4506b78f9b7edfe1fe41cbcfbf77ae3b
IvanBaronov <68464530+IvanBaronov@users.noreply.github.com>
2022-05-06 21:19:54 +0300

    Added AppBar scrolling behavior, text and image in TopFragment and MainActivity Added CollapsingToolbar, image, 2 EditViews, FAB and Snackbar in OrderActivity

--- app/src/main/java/com/example/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/example/bitsandpizzas/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.bitsandpizzas;
 import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -28,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pager);
     }
 
     @Override
@@ -57,4 +69,42 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_TEXT, text);
         shareActionProvider.setShareIntent(intent);
     }
+
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter (FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public Fragment getItem (int position) {
+            switch (position) {
+                case 0: return new TopFragment();
+                case 1: return new PizzaFragment();
+                case 2: return new PastaFragment();
+                case 3: return new StoresFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0: return getResources().getText(R.string.home_tab);
+                case 1: return getResources().getText(R.string.pizza_tab);
+                case 2: return getResources().getText(R.string.pasta_tab);
+                case 3: return getResources().getText(R.string.store_tab);
+            }
+            return null;
+        }
+    }
+
+
+
 }
