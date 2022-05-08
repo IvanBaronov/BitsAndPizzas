e17be7eb65fa271e092287458670f20f28d76499
IvanBaronov <68464530+IvanBaronov@users.noreply.github.com>
2022-05-08 23:58:16 +0300

    Updated PizzaFragment. Replaced ListView with RecyclerView. Pizza names and pictures are now displayed on CardViews as a table. Used GridLayoutManager. Added CaptionedImagesAdapter linking data from Pizza.java file to RecyclerView. Added layout for CardView. Added layout for RecyclrtView.

---
+++ app/src/main/java/com/example/bitsandpizzas/Pizza.java
@@ -0,0 +1,25 @@
+package com.example.bitsandpizzas;
+
+
+public class Pizza {
+    private String name;
+    private int imageResourceId;
+
+    public static final Pizza[] pizzas = {
+            new Pizza("Diavolo", R.drawable.diavolo),
+            new Pizza("Funghi", R.drawable.funghi)
+    };
+
+    private Pizza(String name, int imageResourceId) {
+        this.name = name;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
